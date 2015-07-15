c2551cbb1dbd345cd967fc0a0975235f36cbc9eb
Moritz Kornher <moritz@prefer.co.nz>
2015-07-15 12:26:24 +1200

    Updated config loader

--- public/wp-config.php
+++ public/wp-config.php
@@ -3,15 +3,15 @@
  * The base configurations of the WordPress.
  * Please do not change this file. You can edit your config via
  * - .env or Environmental variables
- * - config.json file
+ * - autobahn.json file
  * - config/*.php files
  */
 
 /** Composer autoload */
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
-/** Make a new WordPress config */
-//new Autobahn\Config\WordPress(dirname(__DIR__));
+/** Make a new WordPress config and save the table prefix */
+$table_prefix = (new Autobahn\Config\WordPress(dirname(__DIR__)))->set()->getConfig('DB_PREFIX');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
